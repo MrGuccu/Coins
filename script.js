9e4fb8a01db8a7a8b113a8e08ff9e32fd30339a5
Mrguccu <mrguccu@gmail.com>
2023-06-10 21:29:35 -0500

    Just a bunch of new stuff

--- script.js
+++ script.js
@@ -41,146 +41,151 @@ toggleButton.addEventListener('click', function() {
 
 // Define an array of coin objects
 const coins = [
-    {
+  {
     origin: "United States",
     denomination: "Standing Liberty Quarter",
-    year: 1925,
-    mint: "P",
-    condition: "Fine",
-    variety: "Chain AMERICA",
-    notes: "Some notes about the coin",
-    obverseImage: "pics/1925 Standing Liberty Quarter Obverse.png",
-    reverseImage: "pics/1925 Standing Liberty Quarter Reverse.png"
-    },
-    {
+    year: 1925
+  },
+  {
     origin: "United States",
     denomination: "Capped Bust Half Dollar",
-    year: 1824,
-    mint: "P",
-    condition: "Very Fine",
-    variety: "None",
-    notes: "Some notes about the coin",
-    obverseImage: "pics/1824 Capped Bust Half Dollar Obverse.png",
-    reverseImage: "pics/1824 Capped Bust Half Dollar Reverse.png"
-    },
-    {
+    year: 1824
+  },
+  {
     origin: "United States",
     denomination: "Capped Bust Half Dollar",
-    year: 1829,
-    mint: "P",
-    condition: "Very Fine",
-    variety: "None",
-    notes: "Some notes about the coin",
-    obverseImage: "pics/1829 Capped Bust Half Dollar Obverse.png",
-    reverseImage: "pics/1829 Capped Bust Half Dollar Reverse.png"
-    },
-    {
-      origin: "United States",
-      denomination: "Large Cent",
-      year: 1798,
-      mint: "P",
-      condition: "Very Fine",
-      variety: "None",
-      notes: "Some notes about the coin",
-      obverseImage: "pics/1798 Large Cent Obverse.png",
-      reverseImage: "pics/1798 Large Cent Reverse.png"
-    },
-    {
-      origin: "United States",
-      denomination: "Large Cent",
-      year: 1800,
-      mint: "P",
-      condition: "Very Fine",
-      variety: "None",
-      notes: "Some notes about the coin",
-      obverseImage: "pics/1800 Large Cent Obverse.png",
-      reverseImage: "pics/1800 Large Cent Reverse.png"
-    },
-    {
-      origin: "United States",
-      denomination: "Large Cent",
-      year: 1801,
-      mint: "P",
-      condition: "Very Fine",
-      variety: "None",
-      notes: "Some notes about the coin",
-      obverseImage: "pics/1801 Large Cent Obverse.png",
-      reverseImage: "pics/1801 Large Cent Reverse.png"
-    },
-    {
-      origin: "United States",
-      denomination: "Large Cent",
-      year: 1802,
-      mint: "P",
-      condition: "Very Fine",
-      variety: "None",
-      notes: "Some notes about the coin",
-      obverseImage: "pics/1802 Large Cent Obverse.png",
-      reverseImage: "pics/1802 Large Cent Reverse.png"
-    },
-    {
-      origin: "United States",
-      denomination: "Large Cent",
-      year: 1803,
-      mint: "P",
-      condition: "Very Fine",
-      variety: "None",
-      notes: "Some notes about the coin",
-      obverseImage: "pics/1803 Large Cent Obverse.png",
-      reverseImage: "pics/1803 Large Cent Reverse.png"
-    },
-    
-    // Add more coin objects here
-    ];
-    
-    // Function to create a coin element
-    function createCoinElement(coin) {
-    const coinElement = document.createElement('div');
-    coinElement.classList.add('coin');
-    
-    const doubleElement = document.createElement('div');
-    doubleElement.classList.add('double');
-    
-    const obverseImage = document.createElement('img');
-    obverseImage.src = coin.obverseImage;
-    obverseImage.alt = 'pics/Alt.png';
-    
-    const reverseImage = document.createElement('img');
-    reverseImage.src = coin.reverseImage;
-    reverseImage.alt = 'pics/Alt.png';
-    
-    doubleElement.appendChild(obverseImage);
-    doubleElement.appendChild(reverseImage);
-    
-    const singleElement = document.createElement('div');
-    singleElement.classList.add('single');
-    
-    const singleImage = document.createElement('img');
-    singleImage.src = coin.obverseImage;
-    singleImage.alt = 'pics/Alt.png';
-    singleImage.onmouseover = function() {
-    this.src = coin.reverseImage;
-    };
-    singleImage.onmouseout = function() {
-    this.src = coin.obverseImage;
-    };
-    
-    singleElement.appendChild(singleImage);
-    
-    const yearElement = document.createElement('p');
-    yearElement.textContent = `${coin.year} - ${coin.denomination}`;
-    
-    coinElement.appendChild(doubleElement);   
-    coinElement.appendChild(singleElement);
-    coinElement.appendChild(yearElement);
-    
-    return coinElement;
-    }
-    
-    // Add coins to the flex container
-    const flexContainer = document.querySelector('.flex-container');
-    
-    coins.forEach(function(coin) {
-    const coinElement = createCoinElement(coin);
-    flexContainer.appendChild(coinElement);
-    });
+    year: 1829
+  },
+  {
+    origin: "United States",
+    denomination: "Large Cent",
+    year: 1798
+  },
+  {
+    origin: "United States",
+    denomination: "Large Cent",
+    year: 1800
+  },
+  {
+    origin: "United States",
+    denomination: "Large Cent",
+    year: 1801
+  },
+  {
+    origin: "United States",
+    denomination: "Large Cent",
+    year: 1802
+  },
+  {
+    origin: "United States",
+    denomination: "Large Cent",
+    year: 1803
+  },
+  {
+    origin: "United States",
+    denomination: "Capped Bust Quarter",
+    year: 1834
+  },
+  {
+    origin: "United States",
+    denomination: "Flying Eagle Cent",
+    year: 1857
+  },
+  {
+    origin: "United States",
+    denomination: "Seated Liberty Half Dollar",
+    year: 1877
+  },
+  {
+    origin: "United States",
+    denomination: "Seated Liberty Quarter",
+    year: 1877
+  },
+  {
+    origin: "United States",
+    denomination: "V Nickel",
+    year: 1883
+  },
+  {
+    origin: "United States",
+    denomination: "VDB Cent",
+    year: 1909
+  },
+  {
+    origin: "Austria Hungary",
+    denomination: "1 Korona",
+    year: 1915
+  },
+  {
+    origin: "United States",
+    denomination: "Peace Dollar",
+    year: 1923
+  },
+  {
+    origin: "Switzerland",
+    denomination: "Swiss 2 Frank",
+    year: 1947
+  },
+  {
+    origin: "Switzerland",
+    denomination: "Swiss 1 Frank",
+    year: 1944
+  },
+  {
+    origin: "Switzerland",
+    denomination: "Swiss 0.5 Frank",
+    year: 1944
+  },
+];
+
+// Function to create a coin element
+function createCoinElement(coin) {
+  const coinElement = document.createElement('div');
+  coinElement.classList.add('coin');
+
+  const doubleElement = document.createElement('div');
+  doubleElement.classList.add('double');
+
+  const obverseImage = document.createElement('img');
+  obverseImage.src = `pics/${coin.year} ${coin.denomination} Obverse.png`;
+  obverseImage.alt = 'pics/Alt.png';
+
+  const reverseImage = document.createElement('img');
+  reverseImage.src = `pics/${coin.year} ${coin.denomination} Reverse.png`;
+  reverseImage.alt = 'pics/Alt.png';
+
+  doubleElement.appendChild(obverseImage);
+  doubleElement.appendChild(reverseImage);
+
+  const singleElement = document.createElement('div');
+  singleElement.classList.add('single');
+
+  const singleImage = document.createElement('img');
+  singleImage.src = `pics/${coin.year} ${coin.denomination} Obverse.png`;
+  singleImage.alt = 'pics/Alt.png';
+  singleImage.onmouseover = function () {
+    this.src = `pics/${coin.year} ${coin.denomination} Reverse.png`;
+  };
+  singleImage.onmouseout = function () {
+    this.src = `pics/${coin.year} ${coin.denomination} Obverse.png`;
+  };
+
+  singleElement.appendChild(singleImage);
+
+  const yearElement = document.createElement('p');
+  yearElement.textContent = `${coin.year} - ${coin.denomination}`;
+
+  coinElement.appendChild(doubleElement);
+  coinElement.appendChild(singleElement);
+  coinElement.appendChild(yearElement);
+
+  return coinElement;
+}
+
+// Add coins to the flex container
+const flexContainer = document.querySelector('.flex-container');
+
+coins.forEach(function (coin) {
+  const coinElement = createCoinElement(coin);
+  flexContainer.appendChild(coinElement);
+});
